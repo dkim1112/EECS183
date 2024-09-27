e5c8c1ad2878daf45bdddc9048cb4bd5168f465b
dkim1112 <de7danielkim@gmail.com>
2024-09-27 15:11:50 -0400

    Finished version of Lab5.

--- Lab5/loops.cpp
+++ Lab5/loops.cpp
@@ -4,10 +4,11 @@
  * EECS 183
  * Lab 5: Loops exercises
  *
- * <#Name(s)#>
- * <#uniqname(s)#>
+ * Dongeun Kim
+ * dongeunk
  *
- * <#A description of the assignment here#>
+ * Utilizing for-loops, we are trying to print various shapes of triangles 
+ * based on the values inputted by the users.
  */
 
 #include "loops.h"
@@ -15,8 +16,50 @@
 
 using namespace std;
 
-/** 
- * TODO
- * Implement all loops.h functions:
- * printRectangle, printRight, printRightJustified, printIsosceles
- */
+void printRectangle(int rows, int cols) {
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<cols; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void printRight(int n) {
+    for (int i=1; i<=n; i++) {
+        for (int j=i; j>0; j--) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void printRightJustified(int n) {
+    for (int i=1; i<=n; i++) {
+        int white = n-i;
+        for (int j=0; j<white; j++) {
+            cout << " ";
+        }
+        for (int k=0; k<i; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void printIsosceles(int n) {
+    int e = 1;
+    for (int i=1; i<=n; i++) {
+        for (int j=n-1; j>=i; j--) {
+            cout << " ";
+        }
+        for (int c=0; c<e; c++) {
+            cout << "*";
+        }
+        cout << endl;
+        e+=2;
+    }
+}
+
+
--- Lab5/test.cpp
+++ Lab5/test.cpp
@@ -4,39 +4,51 @@
  * EECS 183
  * Lab 5: loops exercises
  *
- * <#Name(s)#>
- * <#uniqname(s)#>
+ * Dongeun Kim
+ * dongeunk
  *
- * <#A description of the assignment here#>
+ * This serves as a place to test the functionality of each functions
+ * that has been made in the loops.cpp file.
  */
 
+// This allows us to access the functions we have made in the loops.cpp to test my code.
 #include "loops.h"
 
 #include <iostream>
 using namespace std;
 
 void testPrintRectangle();
+void testPrintRight();
+void testPrintRightJustified();
+void testPrintIsosceles();
 
 int main() {
-    
-    // TODO
-    // Once you have completed the testing functions
-    // call them such that the tests actually get run
     testPrintRectangle();
-
+    testPrintRight();
+    testPrintRightJustified();
+    testPrintIsosceles();
     return 0;
 }
 
 void testPrintRectangle() {
     cout << "Testing printRectangle()" << endl;
     printRectangle(8, 8);
-    
 }
 
-// TODO
-// Implement your other test_functions
+void testPrintRight() {
+    cout << "Testing printRight()" << endl;
+    printRight(3);
+}
 
+void testPrintRightJustified() {
+    cout << "Testing printRightJustified()" << endl;
+    printRightJustified(3);
+}
 
+void testPrintIsosceles() {
+    cout << "Testing printIsosceles()" << endl;
+    printIsosceles(3);
+}
 
 
 
