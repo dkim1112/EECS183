0a1d3f48ac5ff7ed96be1f35c25736c4dadbac17
dkim1112 <de7danielkim@gmail.com>
2024-09-27 14:15:34 -0400

    Initial Commit

---
+++ Lab5/loops.cpp
@@ -0,0 +1,22 @@
+/**
+ * loops.cpp
+ *
+ * EECS 183
+ * Lab 5: Loops exercises
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#A description of the assignment here#>
+ */
+
+#include "loops.h"
+#include <iostream>
+
+using namespace std;
+
+/** 
+ * TODO
+ * Implement all loops.h functions:
+ * printRectangle, printRight, printRightJustified, printIsosceles
+ */
---
+++ Lab5/test.cpp
@@ -0,0 +1,42 @@
+/**
+ * test.cpp
+ *
+ * EECS 183
+ * Lab 5: loops exercises
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#A description of the assignment here#>
+ */
+
+#include "loops.h"
+
+#include <iostream>
+using namespace std;
+
+void testPrintRectangle();
+
+int main() {
+    
+    // TODO
+    // Once you have completed the testing functions
+    // call them such that the tests actually get run
+    testPrintRectangle();
+
+    return 0;
+}
+
+void testPrintRectangle() {
+    cout << "Testing printRectangle()" << endl;
+    printRectangle(8, 8);
+    
+}
+
+// TODO
+// Implement your other test_functions
+
+
+
+
+
