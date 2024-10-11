ea8f0d0770560cbefa97497db47a7a4e8087d30c
dkim1112 <de7danielkim@gmail.com>
2024-10-11 14:58:46 -0400

    final submission

--- Lab6/arrays.cpp
+++ Lab6/arrays.cpp
@@ -7,7 +7,9 @@
  * Dongeun Kim
  * dongeunk
  *
- * <#A description of the assignment here#>
+ * It performs various functions on the 1D array,
+ * such as negating all the values, checking if the 1D array is sorted,
+ * copying positive elments, and even sorting them in numerical order.
  */
 
 #include "arrays.h"
@@ -24,8 +26,52 @@ void print_array(const int array[], int size) {
     return;
 }
 
-/** 
- * TODO
- * Implement all arrays.h functions:
- * negate_all, is_sorted, copy_positive_elements, sort_array
- */
+void negate_all(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = arr[i] * -1;
+    }
+}
+
+bool is_sorted(const int arr[], int size) {
+    bool sorted = true;
+    for (int i = 0; i < size - 1; i++) {
+        if ((arr[i] > arr [i + 1])) {
+            sorted = false;
+            break;
+        }
+    }
+    return sorted;
+}
+
+void copy_positive_elements(const int source[], int target[], int size) {
+    for (int i = 0; i < size; i++) {
+        target[i] = 0;
+    }
+
+    // Copy the positive elements from source to adjacent locations in target
+    int targetIndex = 0; // Tracks the current index in target for positive elements
+    for (int i = 0; i < size; i++) {
+        if (source[i] > 0) {
+            target[targetIndex] = source[i];
+            targetIndex++;
+        }
+    }
+}
+
+void sort_array(int arr[], int size) {
+    bool condition = is_sorted(arr, size);
+    int temp;
+
+    while (!condition) {
+        for (int i = 0; i < size - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+
+                condition = is_sorted(arr, size);
+            }
+        }
+    }
+}
+
--- Lab6/test.cpp
+++ Lab6/test.cpp
@@ -7,7 +7,7 @@
  * Dongeun Kim
  * dongeunk
  *
- * <#A description of the assignment here#>
+ * This file serves as a purpose to check if the functions created in arrays.cpp function as expected.
  */
 
 #include "arrays.h"
@@ -16,28 +16,43 @@
 using namespace std;
 
 void test_negate_all();
+void test_is_sorted();
+void test_copy_positive_elements();
+void test_sort_array();
 
 int main() {
-    
-    // TODO
-    // Once you have completed the testing functions
-    // call them such that the tests actually get run
     test_negate_all();
-
+    test_is_sorted();
+    test_copy_positive_elements();
+    test_sort_array();
     return 0;
 }
 
 void test_negate_all() {
+    int arr[5] = {1,2,3,-4,5};
     cout << "Testing negate_all()" << endl;
-    
-    // TODO implement your test cases for negate_all
+    negate_all(arr, 5);
+        
+}
+
+void test_is_sorted() {
+    int arr[5] = {1,2,3,-4,5};
+    cout << "Testing is_sorted()" << endl;
+    cout << is_sorted(arr, 5);
+}
+
+void test_copy_positive_elements() {
+    int arr[5] = {1,2,3,-4,5};
+    int target[5] = {0, 0, 0, 0, 0};
+    cout << "Testing copy_positive_elements()" << endl;
+    copy_positive_elements(arr, target, 5);
     
 }
 
-// TODO
-// Implement your other test_functions
-
-
-
+void test_sort_array() {
+    int arr[5] = {1,2,3,-4,5};
+    cout << "Testing sort_array()" << endl;
+    sort_array(arr, 5);
+}
 
 
